9751ded3f9645bc1b24472df8b07511cbf58d69c
Matt <wrestler.411@comcast.net>
2018-01-08 10:43:34 -0800

    I think I did it? need unit tests and readme

--- Program.cs
+++ Program.cs
@@ -1,15 +1,20 @@
 using System;
-class Classes
+
+
+class TowerOfHanoi
 {
     int discs;
+
     public TowerOfHanoi()
     {
         numdiscs = 0;
     }
+
     public TowerOfHanoi(int newVal)
     {
         numdiscs = newVal;
     }
+
     public int numdiscs
     {
         get
@@ -23,3 +28,32 @@ class Classes
         }
     }
 
+}
+
+public void moveTower(int n, int from, int to, int vacant)
+{
+    if (n > 0)
+    {
+        movetower(n - 1, from, vacant, to);
+        Console.WriteLine("Move disk {0} from tower {1} to tower {2}", n, from, to);
+        movetower(n - 1, vacant, to, from);
+    }
+}
+
+class Gameplay
+{
+    public static int Main()
+    {
+        TowerOfHanoi Tower = new TowerOfHanoi();
+        string userNumDiscs;
+
+        Console.Write("Enter the number of discs: ");
+        cnumdiscs = Console.ReadLine();
+        Tower.numdiscs = Convert.ToInt32(userNumDiscs);
+
+        Tower.movetower(T.numdiscs, 1, 3, 2);
+        Console.ReadLine();
+
+        return 0;
+    }
+}
